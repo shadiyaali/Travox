9217fdd073309615c9164e5f456a7ab98036b67f
shadiya <118953383+shadiyaali@users.noreply.github.com>
2024-12-23 13:05:01 +0530

    Remove .env from Git tracking

--- travox/settings.py
+++ travox/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-=%ve=^b)!nlwvy^w0w5q7#f%zxdk+bi*h1_w(tushkc!ih^2#p'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 AUTH_USER_MODEL = 'superadmin.User' 
